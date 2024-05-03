9d8ffa08f0c4f9792ac7a1b7097b27e078f2b0aa
Gamingopolis-coding <102993571+Gamingopolis-coding@users.noreply.github.com>
2024-05-03 12:52:52 +0200

    websitemettu

---
+++ mail.php
@@ -0,0 +1,52 @@
+<?php
+	
+	if( empty( $_POST['token'] ) ){
+		echo '<span class="notice">Error!</span>';
+		exit;
+	}
+	if( $_POST['token'] != 'FsWga4&@f6aw' ){
+		echo '<span class="notice">Error!</span>';
+		exit;
+	}
+	
+	$name = $_POST['name'];
+	$from = $_POST['email'];
+	$phone = $_POST['phone'];
+	$subject = stripslashes( nl2br( $_POST['subject'] ) );
+	$message = stripslashes( nl2br( $_POST['message'] ) );
+	
+	$headers ="From: Form Contact <$from>\n";
+	$headers.="MIME-Version: 1.0\n";
+	$headers.="Content-type: text/html; charset=iso 8859-1";
+	
+	ob_start();
+	?>
+		Hoi Mett-u beheerder!<br /><br />
+		<?php echo ucfirst( $name ); ?>  heeft je een bericht geschreven via de website.
+		<br /><br />
+		
+		Name: <?php echo ucfirst( $name ); ?><br />
+		Email: <?php echo $from; ?><br />
+		Phone: <?php echo $phone; ?><br />
+		Subject: <?php echo $subject; ?><br />
+		Message: <br /><br />
+		<?php echo $message; ?>
+		<br />
+		<br />
+		============================================================
+	<?php
+	$body = ob_get_contents();
+	ob_end_clean();
+	
+	$to = 'support@fruitkha.com';
+
+	$s = mail($to,$subject,$body,$headers,"-t -i -f $from");
+
+	if( $s == 1 ){
+		echo '<div class="success"><i class="fas fa-check-circle"></i><h3>Thank You!</h3>Je bericht is succesvol aangekomen.</div>';
+	}else{
+		echo '<div>Er is een fout opgetreden.</div>';
+	}
+
+	
+?>
