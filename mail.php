84a5dfc9c2cc2ae363548b4bed435cf1e7608c48
Gamingopolis-coding <102993571+Gamingopolis-coding@users.noreply.github.com>
2024-09-19 19:16:37 +0200

    Updatemail.php
    qd

--- mail.php
+++ mail.php
@@ -1,49 +1,61 @@
 <?php
-if (empty($_POST['token'])) {
-    echo '<span class="notice">Error!</span>';
-    exit;
-}
-if ($_POST['token'] != 'FsWga4&@f6aw') {  // Zorg ervoor dat dit token overeenkomt met je HTML-formulier
-    echo '<span class="notice">Error!</span>';
+// Tokenvalidatie
+if (empty($_POST['token']) || $_POST['token'] != 'FsWga4&@f6aw') {
+    echo '<span class="notice">Error! Ongeldig token.</span>';
     exit;
 }
 
-$name = $_POST['Naam'];
-$from = $_POST['email'];
-$phone = $_POST['telefoon'];
-$subject = stripslashes(nl2br($_POST['Onderwerp']));
-$message = stripslashes(nl2br($_POST['bericht']));
+// Validatie en sanering van de POST-gegevens
+$name = isset($_POST['Naam']) ? htmlspecialchars(trim($_POST['Naam'])) : '';
+$from = isset($_POST['email']) ? htmlspecialchars(trim($_POST['email'])) : '';
+$phone = isset($_POST['telefoon']) ? htmlspecialchars(trim($_POST['telefoon'])) : '';
+$subject = isset($_POST['Onderwerp']) ? stripslashes(nl2br(htmlspecialchars(trim($_POST['Onderwerp'])))) : '';
+$message = isset($_POST['bericht']) ? stripslashes(nl2br(htmlspecialchars(trim($_POST['bericht'])))) : '';
 
-$headers = "From: Form Contact <$from>\n";
-$headers .= "MIME-Version: 1.0\n";
-$headers .= "Content-type: text/html; charset=iso 8859-1";
+// Controleer of de verplichte velden zijn ingevuld
+if (empty($name) || empty($from) || empty($message)) {
+    echo '<span class="notice">Error! Vul alle verplichte velden in.</span>';
+    exit;
+}
 
+// E-mail validatie
+if (!filter_var($from, FILTER_VALIDATE_EMAIL)) {
+    echo '<span class="notice">Ongeldig e-mailadres!</span>';
+    exit;
+}
+
+// E-mail headers instellen
+$headers = "From: Form Contact <$from>\r\n";
+$headers .= "Reply-To: $from\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+// E-mailinhoud opbouwen
 ob_start();
 ?>
 Hoi METT-U beheerder!<br /><br />
-<?php echo ucfirst($name); ?> heeft je een bericht geschreven via de website.
+<strong><?php echo ucfirst($name); ?></strong> heeft je een bericht geschreven via de website.
 <br /><br />
-
-Naam: <?php echo ucfirst($name); ?><br />
-Email: <?php echo $from; ?><br />
-Telefoon: <?php echo $phone; ?><br />
-Onderwerp: <?php echo $subject; ?><br />
-Bericht: <br /><br />
+<strong>Naam:</strong> <?php echo ucfirst($name); ?><br />
+<strong>Email:</strong> <?php echo $from; ?><br />
+<strong>Telefoon:</strong> <?php echo $phone; ?><br />
+<strong>Onderwerp:</strong> <?php echo $subject; ?><br />
+<strong>Bericht:</strong><br /><br />
 <?php echo $message; ?>
-<br />
-<br />
+<br /><br />
 ============================================================
 <?php
 $body = ob_get_contents();
 ob_end_clean();
 
-$to = 'contact@mett-u.be';  // Verander dit naar je eigen e-mailadres
+// E-mailadres van de ontvanger
+$to = 'contact@mett-u.be';  // Pas dit aan naar je eigen e-mailadres
 
-$s = mail($to, $subject, $body, $headers);
-
-if ($s == 1) {
-    echo '<div class="success"><i class="fas fa-check-circle"></i><h3>Bedankt</h3>Je bericht is succesvol aangekomen.</div>';
+// Verzenden van de e-mail
+if (mail($to, $subject, $body, $headers)) {
+    echo '<div class="success"><i class="fas fa-check-circle"></i><h3>Bedankt</h3>Je bericht is succesvol verstuurd.</div>';
 } else {
-    echo '<div>Er is een fout opgetreden.</div>';
+    echo '<div>Er is een fout opgetreden bij het verzenden van je bericht. Probeer het later opnieuw.</div>';
 }
+
 ?>
